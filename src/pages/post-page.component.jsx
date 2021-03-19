8a566f951bb89be2cd40c2251f21901190de331f
Shivam Singh <SSingh@dealersocket.com>
2021-03-19 09:54:07 +0530

    resolved 404 error on netlify

--- src/pages/post-page.component.jsx
+++ src/pages/post-page.component.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect, Fragment } from "react";
 import { makeStyles } from "@material-ui/styles";
 import { useParams } from "react-router";
 import axios from "axios";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { Footer } from "../components/footer.component";
-import { parseDate } from './../utils/parse-date';
+import { parseDate } from "./../utils/parse-date";
+import { Subscribe } from "./../components/subscribe.component";
 
 export const PostPage = () => {
   const classes = useStyles();
@@ -41,26 +42,33 @@ export const PostPage = () => {
               <h1 className={classes.postTitle}>{post.title}</h1>
               <p className={classes.postSubTitle}>{post.subTitle}</p>
             </Col>
-            <Col style={{paddingLeft: 0, paddingRight: 0}} sm={12} md={5}>
-              <img className={classes.postImage} src={post.postImage} alt=""/>
+            <Col style={{ paddingLeft: 0, paddingRight: 0 }} sm={12} md={5}>
+              <img className={classes.postImage} src={post.postImage} alt="" />
             </Col>
             <Col className={classes.metaContainer} sm={12} md={6}>
               <div className={classes.metaRow}>
-                {
-                  post && post.tags && post.tags.map(tag => (
+                {post &&
+                  post.tags &&
+                  post.tags.map((tag) => (
                     <span className={classes.tags}>{tag.tagName}</span>
-                  ))
-                }
+                  ))}
               </div>
               <div className={classes.metaRow}>
-                { post && post.publishedAt && (<span className={classes.metaSpan}>{parseDate(post.publishedAt)}</span>)}
-                <span className={classes.metaSpan}>{`${post.readTime} minute read`}</span>
+                {post && post.publishedAt && (
+                  <span className={classes.metaSpan}>
+                    {parseDate(post.publishedAt)}
+                  </span>
+                )}
+                <span
+                  className={classes.metaSpan}
+                >{`${post.readTime} minute read`}</span>
               </div>
             </Col>
           </Row>
         </Container>
-        {body.body}
       </div>
+      {body.body}
+      <Subscribe />
       <Footer />
     </Fragment>
   );
@@ -68,47 +76,47 @@ export const PostPage = () => {
 
 const useStyles = makeStyles(() => ({
   postHeader: {
-    backgroundColor:'#1d86b8',
+    backgroundColor: "#1d86b8",
     marginTop: -15,
     paddingTop: 30,
     paddingBottom: 30,
   },
-  postImage:{
-    maxWidth: '100%',
+  postImage: {
+    maxWidth: "100%",
     minHeight: 300,
     borderRadius: 10,
   },
-  postTitle:{
-    color: 'white',
+  postTitle: {
+    color: "white",
     fontSize: 50,
     letterSpacing: 2,
   },
-  postSubTitle:{
-    color: 'white',
+  postSubTitle: {
+    color: "white",
     fontSize: 20,
   },
-  metaContainer:{
-    display: 'flex',
-    flexDirection: 'column',
+  metaContainer: {
+    display: "flex",
+    flexDirection: "column",
   },
-  metaRow:{
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+  metaRow: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 15,
   },
-  tags:{
+  tags: {
     marginRight: 30,
-    color: 'orange',
+    color: "orange",
     fontWeight: 500,
     letterSpacing: 2,
-    border: '2px solid orange',
-    padding: '2px 15px 2px 15px',
+    border: "2px solid orange",
+    padding: "2px 15px 2px 15px",
     borderRadius: 15,
   },
-  metaSpan:{
+  metaSpan: {
     marginRight: 30,
-    color: 'orange',
+    color: "orange",
     fontWeight: 500,
-  }
+  },
 }));
