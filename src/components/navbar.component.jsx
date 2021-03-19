f1f48f0563359d00480c45a8f08b7ab7c0dd9b7d
Shivam Singh <SSingh@dealersocket.com>
2021-03-19 18:27:05 +0530

    corrected nav links

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { makeStyles } from "@material-ui/styles";
+import { Link } from "react-router-dom";
 
 export const NavigationBar = (props) => {
   const classes = useStyles();
   return (
-    <Navbar className={classes.navbar} sticky="top" expand="lg" variant="dark" bg="dark">
-      <Container >
+    <Navbar
+      className={classes.navbar}
+      sticky="top"
+      expand="lg"
+      variant="dark"
+      bg="dark"
+    >
+      <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className={classes.navLink} href="#home">Home</Nav.Link>
-            <Nav.Link className={classes.navLink} href="#videos">Videos</Nav.Link>
-            <Nav.Link className={classes.navLink} href="#videos">About</Nav.Link>
-            <Nav.Link className={classes.navLink} href="#videos">Contact</Nav.Link>
+            <Link className={`${classes.navLink} nav-link`} to="/">
+              Home
+            </Link>
+            <Link className={`${classes.navLink} nav-link`} to="/archive">
+              Archive
+            </Link>
+            <Link className={`${classes.navLink} nav-link`} to="">
+              About Me
+            </Link>
+            <Link className={`${classes.navLink} nav-link`} to="">
+              Contact
+            </Link>
           </Nav>
           <Nav>
-            <Nav.Link className={classes.navLinkRight} href="#home">Sign In/Sign Up</Nav.Link>
+            <Nav.Link className={classes.navLinkRight} href="#home">
+              Sign In/Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -28,24 +45,24 @@ const useStyles = makeStyles(() => ({
   navbar: {
     marginBottom: 15,
   },
-  navLink:{
+  navLink: {
     letterSpacing: 2,
-    color: 'white !important',
+    color: "white !important",
     fontSize: 15,
     fontWeight: 400,
     marginRight: 15,
-    '&:hover': {
-      color: '#1d86b8 !important',
-    }
+    "&:hover": {
+      color: "orange !important",
+    },
   },
-  navLinkRight:{
+  navLinkRight: {
     letterSpacing: 2,
-    color: 'white !important',
+    color: "white !important",
     fontSize: 15,
     fontWeight: 400,
     marginLeft: 15,
-    '&:hover': {
-      color: '#1d86b8 !important',
-    }
-  }
+    "&:hover": {
+      color: "orange !important",
+    },
+  },
 }));
